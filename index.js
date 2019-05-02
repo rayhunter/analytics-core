3d59403e9dbd33e877bef68d0534fcbb56ef7e8d
Raymond Hunter <m752774@a4774356.federated.fds>
2019-05-02 12:49:27 -0700

    updating

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contains all logic for determining analytics tags
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+exports.printMsg = () => console.log('testing analytics-core demo');
